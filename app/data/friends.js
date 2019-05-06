9f813878cfe899cbd4054872abfd90cb5cf188e2
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-05-06 13:28:34 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,87 @@
+var friends = [
+  {
+    name: "Garry",
+    photo: "https://i.ytimg.com/vi/SiVr9DM_EG0/maxresdefault.jpg",
+    scores: [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+  },
+  {
+    name: "Sara",
+    photo:
+      "https://i.cartoonnetwork.com/prismo/apps/icon/Ben10_AR_Icon_Round_370x370.png",
+    scores: [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+  },
+  {
+    name: "John",
+    photo:
+      "https://cdn.pastemagazine.com/www/blogs/lists/2010/05/13/homer_simpson_300.jpg",
+    scores: [3, 5, 1, 2, 3, 5, 4, 1, 2, 3]
+  },
+  {
+    name: "Jane",
+    photo:
+      "http://www.dragoart.com/tuts/pics/8/3151/how-to-draw-rallo-tubbs-from-the-cleveland-show.jpg",
+    scores: [5, 2, 3, 3, 1, 2, 1, 2, 3, 4]
+  },
+  {
+    name: "Mathew",
+    photo:
+      "https://static1.fjcdn.com/comments/I+have+no+lips+so+i+wear+fake+red+ones+_929427a3e88928b288e761ab09618d61.jpg",
+    scores: [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+  },
+  {
+    name: "Maria",
+    photo:
+      "https://i.pinimg.com/originals/8f/b1/62/8fb16268171047d65e4600e544ffcbd6.jpg",
+    scores: [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+  },
+  {
+    name: "AK",
+    photo:
+      "https://asianparent-assets-sg.dexecure.net/wp-content/uploads/sites/12/2018/02/best-cartoon-7.jpg",
+    scores: [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+  },
+  {
+    name: "Janie",
+    photo:
+      "https://png.pngtree.com/element_origin_min_pic/16/08/23/1157bbc46e971dc.jpg",
+    scores: [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+  },
+  {
+    name: "Kelvin",
+    photo:
+      "https://carwad.net/sites/default/files/ben-10-cliparts-119698-6546169.jpg",
+    scores: [1, 5, 1, 3, 5, 1, 4, 3, 2, 1]
+  },
+  {
+    name: "Ruby",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU0U0WU5dB4_6c2Iq4xdzoyFdkTn0vOuBxZExWYyo-7_7M-W4c",
+    scores: [1, 3, 3, 1, 2, 4, 4, 1, 3, 1]
+  },
+  {
+    name: "Sunny",
+    photo:
+      "https://dumielauxepices.net/sites/default/files/customer-clipart-cartoon-495548-5022810.png",
+    scores: [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+  },
+  {
+    name: "Alex",
+    photo:
+      "https://freedesignfile.com/upload/2016/04/Cute-bee-cartoon-vector-illustration-10.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Donald",
+    photo:
+      "https://i.cartoonnetwork.com/prismo/props/chars/ben17_180x180_0.png",
+    scores: [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+  },
+  {
+    name: "Rita",
+    photo:
+      "http://1.bp.blogspot.com/_VDYs6Xdg1MM/TLyQYaBzHMI/AAAAAAAAACI/boxfZK7-Wxg/s1600/kai+lan.jpg",
+    scores: [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+  }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+// Data for "database"
+var friends = require("./app/data/friends.js");
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port.
+app.use(express.static("app/public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Routes
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// Start the server to begin listening
+
+//alternative code
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+// app.listen(process.env.PORT || 3000);
